f180630d80eee27b31317891b269195412c159ae
Felipe-Gonz <fg180899@gmail.com>
2025-07-24 21:58:50 -0600

    Commit init

---
+++ src/main/java/com/registro/lfg/CrudMysqlSB/CrudMysqlSbApplication.java
@@ -0,0 +1,13 @@
+package com.registro.lfg.CrudMysqlSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMysqlSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMysqlSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registro/lfg/CrudMysqlSB/CrudMysqlSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.lfg.CrudMysqlSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMysqlSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
