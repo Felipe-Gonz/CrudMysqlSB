936e31f6ef9484ea44610ed3956b587c7d7a48ea
Felipe-Gonz <fg180899@gmail.com>
2025-08-24 22:57:33 -0600

    Commit finish

--- src/main/java/com/registro/lfg/CrudMysqlSB/service/PersonaService.java
+++ src/main/java/com/registro/lfg/CrudMysqlSB/service/PersonaService.java
@@ -1,12 +1,31 @@
 package com.registro.lfg.CrudMysqlSB.service;
 
+import com.registro.lfg.CrudMysqlSB.model.Persona;
 import com.registro.lfg.CrudMysqlSB.repository.PersonaReposiroty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class PersonaService {
 
     @Autowired
     private PersonaReposiroty personaReposiroty;
+
+    public List<Persona> listarTodas() {
+         return personaReposiroty.findAll();
+    }
+
+    public Persona guardar(Persona persona){
+        return personaReposiroty.save(persona);
+    }
+
+    public Persona obtenerPorId(Long id){
+        return  personaReposiroty.findById(id).orElse(null);
+    }
+
+    public void eliminar(Long id){
+        personaReposiroty.deleteById(id);
+    }
 }
